59c53a94b6654a3d80e2776d2a5657884fe401f5
sank108 <137850649+sank108@users.noreply.github.com>
2024-01-20 20:45:07 +0530

    Youtube_Clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
+import { AppContext } from './context/contextApi';
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import LeftNav from "./components/LeftNav";
+import LeftNavMenu from "./components/LeftNavMenu";
+import SearchResult from "./components/SearchResult";
+import SearchResultVideoCard from "./components/SearchResultVideoCard";
+import SuggestionVideoCard from "./components/SuggestionVideoCard";
+import VideoCard from "./components/VideoCard";
+import VideoDetails from "./components/VideoDetails";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext>
+      <BrowserRouter>
+      <div className='flex flex-col h-full'>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Feed/>}/>
+          <Route path="/searchResult/:searchQuery" element={<SearchResult/>}/>
+          <Route path="/video/:id" element={<VideoDetails/>}/>
+
+
+        </Routes>
+      </div>
+      </BrowserRouter>
+    </AppContext>
   );
 }
 
